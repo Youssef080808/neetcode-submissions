de6ec9cc22610d4ab7eefb1506256df2bbe32e92
Youssef080808 <youssef.yasser.mansour@gmail.com>
2026-07-23 23:16:54 +0300

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashMap<Integer, boolean[]> rows = new HashMap<>();
+        HashMap<Integer, boolean[]> cols = new HashMap<>();
+        HashMap<Integer, boolean[]> sq = new HashMap<>();
+        for (int i = 0; i < 9; i++){
+            rows.put(i, new boolean[10]);
+            cols.put(i, new boolean[10]);
+            sq.put(i, new boolean[10]);
+        }
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                // checks if empty space
+                if (board[i][j] == '.') continue;
+                int num = board[i][j] - '0';
+                //checks colomns
+                if (cols.get(j)[num]) return false;
+                cols.get(j)[num] = true;
+                // checks rows
+                if (rows.get(i)[num]) return false;
+                rows.get(i)[num] = true;
+                // checks squares
+                int row = i/3;
+                int col = j/3;
+                int f = row * 3 + col;
+                if (sq.get(f)[num]) return false;
+                sq.get(f)[num] = true;
+            }
+        }
+        return true;
+    }
+}
